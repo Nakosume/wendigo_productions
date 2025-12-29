90e7550f8c41c3d4b05a2a23c93ddf96e8803f40
Sebastian Romero <53229969+Nakosume@users.noreply.github.com>
2025-12-29 10:55:30 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ export default function App () { // Components 101, make function
   return ( // return whatever the component may be using HTML, <> </> this be called a fragment, its an empty tag lol makes it clean tho
     <BrowserRouter>
       <Routes>
-        <Route index element={<Landing />} />
-        <Route path='aboutUs' element={<AboutUs />} />
-        <Route path='contactUs' element={<ContactUs />} />
-        <Route path='projects' element={<Projects />} />
-        <Route path='projects/:id' element={<ProjectDetails />} />
-        <Route path='logIn' element={<LogIn />} />
-        <Route path='admin' element={<Admin />} />
+        <Route path='wendigo_productions/' ={<Landing />} />
+        <Route path='wendigo_productions/aboutUs' element={<AboutUs />} />
+        <Route path='wendigo_productions/contactUs' element={<ContactUs />} />
+        <Route path='wendigo_productions/projects' element={<Projects />} />
+        <Route path='wendigo_productions/projects/:id' element={<ProjectDetails />} />
+        <Route path='wendigo_productions/logIn' element={<LogIn />} />
+        <Route path='wendigo_productions/admin' element={<Admin />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
